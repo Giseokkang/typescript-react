e42a85baf95756516618d624e9f08a91e169a6ad
Giseokkang <river.key93@gmail.com>
2019-08-16 15:57:49 +0900

    how to make Event

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
 import React from "react";
+import Styled from "styled-components";
+import Number from "./Number";
+import { Input, Form } from "./Input";
 
 interface IState {
   counter: number;
+  name: string;
 }
 
-class App extends React.Component {
+class App extends React.Component<{}, IState> {
+  state = {
+    counter: 0,
+    name: "key"
+  };
+
   render() {
-    return <span>Hello</span>;
+    const { counter, name } = this.state;
+    return (
+      <div>
+        <Number count={counter} />
+        <Form onFormSubmit={this.onFormSubmit}>
+          <Input value={name} onChange={this.onChange} />
+        </Form>
+      </div>
+    );
   }
+
+  onChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    {
+      console.log(event.target);
+    }
+  };
+
+  onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
+  add = (): void => {
+    this.setState(prev => {
+      return {
+        counter: prev.counter + 1
+      };
+    });
+  };
 }
 
 export default App;
